1069ecc6307b4e45db7c874ad31b82a5b8ea02ad
Newton Duarte Cavalcanti <newton_@hotmail.com>
2021-07-10 05:39:45 -0300

    refactor: commands script with options and transition controller

--- Assets/Scripts/Commands.cs
+++ Assets/Scripts/Commands.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Commands : MonoBehaviour
 {
-    public void loadScene(int scene)
+    TransitionController _transitionController;
+    OptionsController _optionsController;
+
+    private void Start()
     {
-        SceneManager.LoadScene(scene);
+        _transitionController = FindObjectOfType(typeof(TransitionController)) as TransitionController;
+        _optionsController = FindObjectOfType(typeof(OptionsController)) as OptionsController;
+    }
+
+    public void startGame()
+    {
+        _optionsController.StartCoroutine(_optionsController.changeMusic(_optionsController.startClip));
+        _transitionController.startFade(2);
+    }
+
+    public void titleScreen()
+    {
+        _transitionController.startFade(1);
     }
 
     public void quitGame()
